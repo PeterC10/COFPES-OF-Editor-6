4ebd34ea7c173f52554dcf6a4b8f42b20b764296
Peter Clark <peterc1092@gmail.com>
2020-12-15 23:26:45 +0000

    Added Glasses/Necklace Type attributes
    Linking Glasses/Necklace Type attribute added, with child Glasses Type and Necklace Type attirbutes.
    
    Story/1015
    
    Signed-off-by: Peter Clark <peterc1092@gmail.com>

---
+++ editor/attributes/player/player_attribute_glasses_necklace_type.py
@@ -0,0 +1,104 @@
+from bidict import bidict
+
+from editor.attributes.player.player_attribute import (
+    PlayerAttribute,
+    PlayerAttributeTypes,
+)
+
+from editor.attributes.player.player_attribute_glasses_type import (
+    PlayerAttributeGlassesType,
+)
+
+from editor.attributes.player.player_attribute_necklace_type import (
+    PlayerAttributeNecklaceType,
+)
+
+
+class PlayerAttributeGlassesNecklaceType(PlayerAttribute):
+    opt_n = "N"
+    opt_1 = "1"
+    opt_2 = "2"
+
+    @classmethod
+    def att_class_name(cls):
+        return "Glasses/Necklace Type"
+
+    @classmethod
+    def att_class_type(cls):
+        return PlayerAttributeTypes.Appearance
+
+    @classmethod
+    def att_class_hex_info(cls):
+        return None
+
+    @classmethod
+    def att_class_array_pos(cls):
+        return 97
+
+    @classmethod
+    def att_class_array_opts(cls):
+        """
+        Glasses/Necklace Type opts - N, 1, 2
+        """
+        # Note: 3 is not valid in-game
+        options_by_value = bidict(
+            {
+                0: (cls.opt_n, cls.opt_n),
+                1: (cls.opt_1, cls.opt_n),
+                2: (cls.opt_2, cls.opt_n),
+                4: (cls.opt_n, cls.opt_1),
+                5: (cls.opt_1, cls.opt_1),
+                6: (cls.opt_2, cls.opt_1),
+                8: (cls.opt_n, cls.opt_2),
+                9: (cls.opt_1, cls.opt_2),
+                10: (cls.opt_2, cls.opt_2),
+            }
+        )
+        return options_by_value
+
+    @classmethod
+    def att_class_hidden(cls):
+        return True
+
+    def get_raw_value(self):
+        """
+        Get byte value currently set in player's byte array
+        """
+        of_data = self.player.option_file.data
+        value = of_data[self.player.address + self.array_pos]
+        return value
+
+    def get_value(self):
+        """
+        Get byte value via `get_raw_value`.
+
+        Apply modulo 12 to byte value (to be used to identify label)
+        """
+        value = self.get_raw_value()
+        value = value % 12
+        return value
+
+    def get_label(self):
+        value = self.get_value()
+        label = self.array_opts[value]
+        return label
+
+    def set_value(self, value):
+        of_data = self.player.option_file.data
+        of_data[self.player.address + self.array_pos] = value
+        return True
+
+    def set_value_from_label(self, label):
+        value = self.array_opts.inverse[label]
+        self.set_value(value)
+        return True
+
+    def create_child_attributes(self):
+        """
+        Create Glasses Type and Necklace Type attributes
+        and link to this attribute
+        """
+        self.glasses_type = PlayerAttributeGlassesType(self.player, parent=self)
+        self.necklace_type = PlayerAttributeNecklaceType(
+            self.player, parent=self
+        )
---
+++ editor/attributes/player/player_attribute_necklace_type.py
@@ -0,0 +1,41 @@
+from editor.attributes.player.player_attribute import (
+    PlayerAttribute,
+    PlayerAttributeTypes,
+)
+
+
+class PlayerAttributeNecklaceType(PlayerAttribute):
+    option_n = "N"
+    option_1 = 1
+    option_2 = 2
+
+    @classmethod
+    def att_class_name(cls):
+        return "Necklace Type"
+
+    @classmethod
+    def att_class_type(cls):
+        return PlayerAttributeTypes.Appearance
+
+    def get_raw_value(self):
+        return self.parent.get_value()
+
+    def get_value(self):
+        return self.parent.get_value()
+
+    def get_label(self):
+        """
+        Get full label from parent
+        and return first value (necklace type is set second)
+        """
+        full_label = self.parent.get_label()
+        return full_label[1]
+
+    def set_value(self, value):
+        return self.parent.set_value(value)
+
+    def set_value_from_label(self, label):
+        glasses_type_label = self.parent.glasses_type.get_label()
+        full_label = (glasses_type_label, label)
+
+        return self.parent.set_value_from_label(full_label)
