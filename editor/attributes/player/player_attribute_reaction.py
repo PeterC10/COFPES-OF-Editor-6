b18640bac0dfcc73d7355a5daaf517838ec09a9f
Peter Clark <peterc1092@gmail.com>
2021-02-07 01:34:31 +0000

    Reaction and Freekick Accuracy player attributes added
    Story/1015
    
    Signed-off-by: Peter Clark <peterc1092@gmail.com>

---
+++ editor/attributes/player/player_attribute_reaction.py
@@ -0,0 +1,37 @@
+from editor.attributes.player.player_attribute import (
+    PlayerAttribute,
+    PlayerAttributeTypes,
+)
+
+
+class PlayerAttributeReaction(PlayerAttribute):
+    @classmethod
+    def att_class_name(cls):
+        return "Reaction"
+
+    @classmethod
+    def att_class_type(cls):
+        return PlayerAttributeTypes.SpecialAbilities
+
+    def get_raw_value(self):
+        return self.parent.get_value()
+
+    def get_value(self):
+        return self.parent.get_value()
+
+    def get_label(self):
+        """
+        Get full label from parent
+        and return first value (reaction is set first)
+        """
+        full_label = self.parent.get_label()
+        return full_label[0]
+
+    def set_value(self, value):
+        return self.parent.set_value(value)
+
+    def set_value_from_label(self, label):
+        fk_accuracy_label = self.parent.fk_accuracy.get_label()
+        full_label = (label, fk_accuracy_label)
+
+        return self.parent.set_value_from_label(full_label)
