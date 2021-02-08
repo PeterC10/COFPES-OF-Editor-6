3028ff40166d7a92e3cb71fc099429a00e0b953c
Peter Clark <peterc1092@gmail.com>
2021-02-08 00:17:37 +0000

    CB and Body Balance player attributes added
    Story/1015
    
    Signed-off-by: Peter Clark <peterc1092@gmail.com>

---
+++ editor/attributes/player/player_attribute_body_balance.py
@@ -0,0 +1,37 @@
+from editor.attributes.player.player_attribute import (
+    PlayerAttribute,
+    PlayerAttributeTypes,
+)
+
+
+class PlayerAttributeBodyBalance(PlayerAttribute):
+    @classmethod
+    def att_class_name(cls):
+        return "Body Balance"
+
+    @classmethod
+    def att_class_type(cls):
+        return PlayerAttributeTypes.StandardAbilities
+
+    def get_raw_value(self):
+        return self.parent.get_value()
+
+    def get_value(self):
+        return self.parent.get_value()
+
+    def get_label(self):
+        """
+        Get full label from parent
+        and return second value (body balance is set second)
+        """
+        full_label = self.parent.get_label()
+        return full_label[1]
+
+    def set_value(self, value):
+        return self.parent.set_value(value)
+
+    def set_value_from_label(self, label):
+        cb_label = self.parent.cb.get_label()
+        full_label = (cb_label, label)
+
+        return self.parent.set_value_from_label(full_label)
